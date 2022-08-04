428a7a485c2f2fba4b0863a913f97a1adad061af
河田鷹介 <benefium@gmail.com>
2022-08-04 11:14:33 +0900

    ヘッダファイル

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
-tetris: tetris.c
-	gcc tetris.c -lncurses -o tetris
+NAME	:=	tetris
+CFLAGS	:=
+
+SRCS	:=	tetris.c
+OBJS	:=	$(SRCS:.c=.o)
+
+RM		:= 	rm -rf
+
+.PHONY:	all clean fclean re
+
+all: $(NAME)
+
+$(NAME):	$(OBJS)
+	gcc $(CFLAGS) -lncurses -o $@ $^
+
+clean:
+	$(RM) $(OBJS)
+
+fclean:	clean
+	$(RM) $(NAME)
+
+re:		fclean all
---
+++ types.h
@@ -0,0 +1,35 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+#include <ncurses.h>
+
+#define R 20
+#define C 15
+#define KEY_QUICKEN		's'
+#define KEY_MOVE_RIGHT	'd'
+#define KEY_MOVE_LEFT	'a'
+#define KEY_ROTATE		'w'
+
+typedef char t_board[R][C];
+
+typedef struct s_shape {
+	char **array;
+	int width, row, col;
+} t_shape;
+
+typedef struct s_game
+{
+	t_board		board;
+	suseconds_t	timer;
+	bool		on;
+	int			final;
+	int			decrease;
+}	t_game;
+
+#endif
