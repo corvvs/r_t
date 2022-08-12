57cd6649227a227db9602c40b4b4e49661007318
河田鷹介 <benefium@gmail.com>
2022-08-12 14:19:46 +0900

    説明変数

--- key_action.c
+++ key_action.c
@@ -3,53 +3,63 @@
 #include "shape.h"
 #include "key_action.h"
 
+// ブロックで埋まった行を削除し, 削除した行の数を返す
 static int	remove_filled_lines(t_game *game) {
-	int count = 0;
+	int n_removed_lines = 0;
 	for (int n = 0; n < R; n++) {
 		int blocks = 0;
 		for (int m = 0; m < C; m++) {
 			blocks += game->board[n][m];
 		}
-		if (blocks == C) {
-			count++;
-			int k;
-			for (k = n; k >= 1; k--)
-				memcpy(game->board[k], game->board[k - 1], sizeof(char) * C);
-			memset(game->board[k], 0, sizeof(char) * C);
-			game->turn_duration -= game->duration_decreasement;
-			game->duration_decreasement -= 1;
-		}
+		if (blocks != C) { continue; }
+
+		n_removed_lines += 1;
+		int k;
+		for (k = n; k >= 1; k--)
+			memcpy(game->board[k], game->board[k - 1], sizeof(char) * C);
+		memset(game->board[k], 0, sizeof(char) * C);
+		game->turn_duration -= game->duration_decreasement;
+		game->duration_decreasement -= 1;
 	}
-	return count;
+	return n_removed_lines;
 }
 
 static bool	check_collision(t_board board, const t_shape *shape)
 {
 	char **array = shape->array;
-	for (int i = 0; i < shape->width; i++) {
-		for (int j = 0; j < shape->width; j++) {
-			if ((shape->col + j < 0 || shape->col + j >= C || shape->row + i >= R)) {
-				if (array[i][j])
-					return true;
-			} else if (board[shape->row + i][shape->col + j] && array[i][j])
+	for (int i_shape = 0; i_shape < shape->width; i_shape += 1) {
+		for (int j_shape = 0; j_shape < shape->width; j_shape += 1) {
+			const bool	is_shape_filled = !!array[i_shape][j_shape];
+			if (!is_shape_filled) { continue; }
+			const int	i_board = i_shape + shape->row;
+			const int	j_board = j_shape + shape->col;
+			const bool	is_out_of_bounds = (j_board < 0 || C <= j_board || R <= i_board);
+			if (is_out_of_bounds) {
+				// アウト: shape の埋まっている位置が board からはみ出している(ただし上へのはみ出しは不問)
 				return true;
+			}
+			const bool	is_board_filled = !!board[i_board][j_board];
+			if (is_board_filled) {
+				// アウト: shape と board で同じ位置が埋まっている
+				return true;
+			}
 		}
 	}
 	return false;
 }
 
+// ブロックを落下させ, それにより消去された行の総数を返す
 static int	move_down_shape(t_game *game, t_shape* temp, t_shape* current)
 {
 	temp->row++;
 	if (!check_collision(game->board, temp)) {
 		current->row++;
 		return 0;
-	} else {
-		place_shape_to_board(game->board, current);
-		int removed_lines = remove_filled_lines(game);
-		drop_new_shape(game, current);
-		return removed_lines;
 	}
+	place_shape_to_board(game->board, current);
+	int removed_lines = remove_filled_lines(game);
+	drop_new_shape(game, current);
+	return removed_lines;
 }
 
 void	place_shape_to_board(t_board board, const t_shape* shape)
@@ -75,7 +85,6 @@ void	perform_quicken(t_game *game, t_shape *current)
 	int removed_lines = move_down_shape(game, &temp, current);
 	game->score += 100 * removed_lines;
 	destroy_shape(&temp);
-
 }
 
 void	perform_move_right(t_game *game, t_shape *current)
